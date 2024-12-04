4b2d12c9942dfa33b93cb1809a7f83355ebcfd30
VinsG <162541638+VinsGarniel@users.noreply.github.com>
2024-12-04 23:18:10 +0800

    Add files via upload

---
+++ dcs_enrollment_system/authentication/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.template.loader import get_template
+from admin_dashboard import views
+
+import datetime
+def land(request):
+    return render(request, "authentication/land.html",{})
+def login(request):
+    return render(request, "authentication/login.html",{})
+def home(request):
+    template = loader.get_template('admin_dashboard/index.html')
+    context = {
+        'allstudents':views.allstudents,
+        'number_of_students_enrolled':views.number_of_students_enrolled
+    }
+
+    return HttpResponse(template.render(context, request))
+
+def logout(request):
+    pass

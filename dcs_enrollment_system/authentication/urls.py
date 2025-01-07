1948c9a6a31c01d6c23b7ee142bac6eb13837c1e
luigazab <luishagailcanonoy@gmail.com>
2025-01-07 18:44:41 +0800

    Deleted extra folder
    Folder contained an exact copy of the project folder.

--- dcs_enrollment_system/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    
-    path("", views.land, name="authentication-land"),
-    path("login/", views.login, name="authentication-login"),
-    path("home/", views.home, name="home"),
-    
-]
